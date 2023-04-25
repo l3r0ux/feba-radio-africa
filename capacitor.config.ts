6feaf50160f0a6a5ad0d60c93cf73acfb4d7d830
Le Roux Vermeulen <lerouxvermeulen@gmail.com>
2023-04-25 08:28:31 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'feba_radio_sa',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
