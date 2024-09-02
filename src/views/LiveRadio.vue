c5c968c40c24aa9a652b78cdda677029cc0f18f8
Le Roux Vermeulen <lerouxvermeulen@gmail.com>
2024-09-02 16:51:50 +0200

    fix: litala FM radio url not working

--- src/views/LiveRadio.vue
+++ src/views/LiveRadio.vue
@@ -21,74 +21,78 @@
 </template>
 
 <script setup lang="ts">
-import { IonPage } from '@ionic/vue';
-import Navbar from '@/components/Navbar.vue';
-import RadioTile from '@/components/RadioTile.vue';
-import { ref } from 'vue';
+import { IonPage } from "@ionic/vue";
+import Navbar from "@/components/Navbar.vue";
+import RadioTile from "@/components/RadioTile.vue";
+import { ref } from "vue";
 
 interface RadioTile {
-  imageUrl: string,
-  stationName?: string,
-  country: string,
-  radioUrl: string
+  imageUrl: string;
+  stationName?: string;
+  country: string;
+  radioUrl: string;
 }
 
-let playingRadios = ref<string[]>([])
+let playingRadios = ref<string[]>([]);
 
-const setPlayingRadio = ({ playingRadio, pausingRadio }: {
-  playingRadio: string,
-  pausingRadio: string,
+const setPlayingRadio = ({
+  playingRadio,
+  pausingRadio,
+}: {
+  playingRadio: string;
+  pausingRadio: string;
 }): void => {
-  playingRadios.value.push(playingRadio)
+  playingRadios.value.push(playingRadio);
 
-  if (pausingRadio)
-    playingRadios.value.splice(0, 1)
+  if (pausingRadio) playingRadios.value.splice(0, 1);
 
   if (playingRadios.value.length === 2) {
-    const prevAudioEl: HTMLAudioElement | null = document.querySelector(`.${playingRadios.value[0]}`)
-    prevAudioEl?.pause()
-    playingRadios.value.splice(0, 1)
+    const prevAudioEl: HTMLAudioElement | null = document.querySelector(
+      `.${playingRadios.value[0]}`
+    );
+    prevAudioEl?.pause();
+    playingRadios.value.splice(0, 1);
   }
-}
+};
 
 const radioTiles: Array<RadioTile> = [
   {
-    imageUrl: '/images/malawi_flag.png',
-    country: 'Malawi',
-    stationName: 'Litala FM',
-    radioUrl: 'http://r103.netradio.pro:2000/main',
+    imageUrl: "/images/malawi_flag.png",
+    country: "Malawi",
+    stationName: "Litala FM",
+    radioUrl: "https://live.netradio.pro/listen/litalafm/radio.mp3",
   },
   {
-    imageUrl: '/images/cambodia_flag.png',
-    country: 'Cambodia',
-    radioUrl: 'http://s14.myradiostream.com:10064/;',
+    imageUrl: "/images/cambodia_flag.png",
+    country: "Cambodia",
+    radioUrl: "http://s14.myradiostream.com:10064/;",
   },
   {
-    imageUrl: '/images/ukraine_flag.png',
-    country: 'Ukraine',
-    radioUrl: 'https://stream.radiom.ua/mp3_high',
+    imageUrl: "/images/ukraine_flag.png",
+    country: "Ukraine",
+    radioUrl: "https://stream.radiom.ua/mp3_high",
   },
   {
-    imageUrl: '/images/russia_flag.png',
-    country: 'Russia',
-    radioUrl: 'http://stream.teos.fm:8004/mp3_hq',
+    imageUrl: "/images/russia_flag.png",
+    country: "Russia",
+    radioUrl: "http://stream.teos.fm:8004/mp3_hq",
   },
   {
-    imageUrl: '/images/china_flag.png',
-    country: 'China',
-    radioUrl: 'http://ly729.out.airtime.pro:8000/ly729_b',
+    imageUrl: "/images/china_flag.png",
+    country: "China",
+    radioUrl: "http://ly729.out.airtime.pro:8000/ly729_b",
   },
   {
-    imageUrl: '/images/korea_flag.png',
-    country: 'Korea',
-    radioUrl: 'http://mlive2.febc.net:1935/live/seoulfm/playlist.m3u8',
+    imageUrl: "/images/korea_flag.png",
+    country: "Korea",
+    radioUrl: "http://mlive2.febc.net:1935/live/seoulfm/playlist.m3u8",
   },
   {
-    imageUrl: '/images/philippines_flag.png',
-    country: 'Philippines',
-    radioUrl: 'http://icecast.eradioportal.com:8000/febc_dzas',
+    imageUrl: "/images/philippines_flag.png",
+    country: "Philippines",
+    radioUrl: "http://icecast.eradioportal.com:8000/febc_dzas",
   },
-]
+];
 </script>
 
 <style lang="css" scoped>
